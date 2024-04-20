b916d0d8b920e948ca42ac848cada016f12355c9
rabby-web <rabby.webdeveloper@gmail.com>
2024-04-20 08:03:36 +0600

    post menu item backend

--- index.js
+++ index.js
@@ -131,6 +131,13 @@ async function run() {
       const result = await menuCollection.find().toArray();
       res.send(result);
     });
+
+    app.post("/menu", async (req, res) => {
+      const item = req.body;
+      const result = await menuCollection.insertOne(item);
+      res.send(result);
+    });
+
     // menu get
     app.get("/reviews", async (req, res) => {
       const result = await reviewsCollection.find().toArray();
